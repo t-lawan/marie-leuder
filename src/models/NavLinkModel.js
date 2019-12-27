2f6807bf148748577155c29cba4fa5ddfdeb47ed
akinsola <thomas.lawanson@gmail.com>
2019-12-27 23:38:24 +0000

    created models

---
+++ src/models/NavLinkModel.js
@@ -0,0 +1,8 @@
+export class NavLinkModel {
+    constructor(id, title, external_link, page_id) {
+        this.id = id;
+        this.title = title;
+        this.external_link = external_link;
+        this.page_id = page_id;
+    }
+}
---
+++ src/models/PageModel.js
@@ -0,0 +1,8 @@
+export class PageModel {
+    constructor(id, title, template, content) {
+        this.id = id;
+        this.title = title;
+        this.template = template;
+        this.content = content;
+    }
+}
