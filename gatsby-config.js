6be56f40dcb072b255a076247a0e5c7cce65faee
akinsola <thomas.lawanson@gmail.com>
2019-12-28 01:15:22 +0000

    installed gatsbt config

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,7 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-netlify`,
+    `gatsby-plugin-transition-link`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
