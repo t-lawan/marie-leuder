fe686454888e908e246026478321a60024c446c7
akinsola <thomas.lawanson@gmail.com>
2019-12-29 13:24:58 +0000

    updated models

--- src/models/NavLinkModel.js
+++ src/models/NavLinkModel.js
@@ -1,8 +1,10 @@
 export class NavLinkModel {
-    constructor(id, title, external_link, page_id) {
+    constructor(id, title, external_link, page_id, slug, active = false) {
         this.id = id;
         this.title = title;
         this.external_link = external_link;
         this.page_id = page_id;
+        this.active = active;
+        this.slug = slug;
     }
 }
--- src/models/PageModel.js
+++ src/models/PageModel.js
@@ -1,7 +1,8 @@
 export class PageModel {
-    constructor(id, title, template, content) {
+    constructor(id, title, slug, template, content) {
         this.id = id;
         this.title = title;
+        this.slug = slug;
         this.template = template;
         this.content = content;
     }
