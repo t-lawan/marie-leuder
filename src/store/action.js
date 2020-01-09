ad346103e77845334625077ae3a1028c91fb9ffe
akinsola <thomas.lawanson@gmail.com>
2020-01-09 18:57:52 +0000

    updated store

--- src/store/action.js
+++ src/store/action.js
@@ -1,4 +1,5 @@
 export const SET_PAGES = "SET_PAGES";
 export const SET_NAVBAR_LINKS = "SET_NAVBAR_LINKS";
 export const SET_VIDEOS = 'SET_VIDEOS';
+export const SET_CURRENT_VIDEO = 'SET_CURRENT_VIDEO';
 export const IS_LOADED = "IS_LOADED";
