42880a070ee20125dbe832b5d7af183eab091f46
akinsola <thomas.lawanson@gmail.com>
2020-01-11 19:06:42 +0000

    update

--- src/components/videos/videos.js
+++ src/components/videos/videos.js
@@ -1,20 +1,28 @@
 import React from "react"
 import styled from "styled-components"
 import { connect } from "react-redux"
-import { size } from "../../index.styles";
+import { size } from "../../index.styles"
 
 const VideosWrapper = styled.section`
+  display: none;
   @media (max-width: ${size.tablet}) {
     display: ${props => (props.showInMobile ? "inherit" : "none")};
   }
+  text-align: center;
+`
+
+const Video = styled.video`
+  width: 85%;
+  padding: 1rem;
+  margin: auto;
 `
 const Videos = props => {
   return (
     <VideosWrapper showInMobile={props.showInMobile}>
       {props.videos.map((video, index) => (
-        <p key={index}> 
-            {video.title}
-         </p>
+        <Video key={index}>
+          <source src={video.url} type="video/mp4" />
+        </Video>
       ))}
     </VideosWrapper>
   )
