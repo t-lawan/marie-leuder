9792eb5d8fe568a13b91030c0ad650977ede76c0
akinsola <thomas.lawanson@gmail.com>
2020-01-15 20:05:32 +0000

    not creating pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -11,46 +11,46 @@ const slash = require(`slash`)
 exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
 
-  const result = await graphql(`
-    {
-      allContentfulPage {
-        edges {
-          node {
-            contentful_id
-            title
-            slug
-            template
-            content {
-              json
-            }
-          }
-        }
-      }
-    }
-  `)
+  // const result = await graphql(`
+  //   {
+  //     allContentfulPage {
+  //       edges {
+  //         node {
+  //           contentful_id
+  //           title
+  //           slug
+  //           template
+  //           content {
+  //             json
+  //           }
+  //         }
+  //       }
+  //     }
+  //   }
+  // `)
 
-  if (result.errors) {
-    throw new Error(result.errors)
-  }
-  const { allContentfulPage } = result.data
+  // if (result.errors) {
+  //   throw new Error(result.errors)
+  // }
+  // const { allContentfulPage } = result.data
 
-  const defaultTemplate = path.resolve(`./src/templates/default.js`)
+  // const defaultTemplate = path.resolve(`./src/templates/default.js`)
 
-  allContentfulPage.edges.forEach(edge => {
-    let template
-    switch (edge.node.template) {
-      case "default":
-        template = defaultTemplate
-        break
-      default:
-        template = defaultTemplate
-        break;
-    }
+  // allContentfulPage.edges.forEach(edge => {
+  //   let template
+  //   switch (edge.node.template) {
+  //     case "default":
+  //       template = defaultTemplate
+  //       break
+  //     default:
+  //       template = defaultTemplate
+  //       break;
+  //   }
 
-    createPage({
-      path: edge.node.slug,
-      component: slash(template),
-      context: edge.node,
-    })
-  })
+  //   createPage({
+  //     path: edge.node.slug,
+  //     component: slash(template),
+  //     context: edge.node,
+  //   })
+  // })
 }
