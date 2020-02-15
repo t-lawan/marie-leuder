9381e5180a943adeb97a9ff5d02edcec4dbd9fb7
akinsola <thomas.lawanson@gmail.com>
2020-02-15 00:24:25 +0000

    open in new tab

--- src/utils/richtext.js
+++ src/utils/richtext.js
@@ -1,11 +1,18 @@
 import React from "react"
-import { BLOCKS, MARKS } from '@contentful/rich-text-types';
+import { BLOCKS, MARKS, INLINES } from '@contentful/rich-text-types';
+import styled from 'styled-components';
 
+const HEADING_TWO = styled.h4`
+  font-style: initial;
+  font-weight: normal;
+`
 export const richTextOptions = {
     renderMark: {
       [MARKS.BOLD]: text => <strong>{text}</strong>
     },
     renderNode: {
-      [BLOCKS.PARAGRAPH]: (node, children) => <p>{children}</p>
+      [BLOCKS.PARAGRAPH]: (node, children) => <p>{children}</p>,
+      [BLOCKS.HEADING_4]: (node, children) => <HEADING_TWO> {children} </HEADING_TWO>,
+      [INLINES.HYPERLINK]: (node, children) => <a href={node.data.uri} target="__blank">{children}</a>
     }
   };
